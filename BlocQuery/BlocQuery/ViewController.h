8cfb2b3aa6655ae37adc50ba25dc3c0126aa49c4
mdougla3 <McCay@mccays-mbp.attlocal.net>
2015-09-29 21:05:07 -0500

    Imported <Parse.h>

---
+++ BlocQuery/BlocQuery/ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  BlocQuery
+//
+//  Created by McCay Barnes on 9/29/15.
+//  Copyright © 2015 McCay Barnes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <Parse.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
