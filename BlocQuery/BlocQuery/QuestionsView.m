e1c411cf0715a649709d935f268d75c5d74ad0bf
mdougla3 <McCay@mccays-macbook-pro.local>
2015-10-26 22:06:12 -0500

    Ability to add questions and delete questions. No UI

---
+++ BlocQuery/BlocQuery/QuestionsView.m
@@ -0,0 +1,13 @@
+//
+//  QuestionsView.m
+//  BlocQuery
+//
+//  Created by McCay Barnes on 10/26/15.
+//  Copyright © 2015 McCay Barnes. All rights reserved.
+//
+
+#import "QuestionsView.h"
+
+@implementation QuestionsView
+
+@end
--- BlocQuery/BlocQuery/ViewController.m
+++ BlocQuery/BlocQuery/ViewController.m
@@ -8,11 +8,25 @@
 
 #import "ViewController.h"
 
-@interface ViewController () <PFLogInViewControllerDelegate>
+@interface ViewController () <PFLogInViewControllerDelegate, UITableViewDelegate, UITableViewDataSource, UITextFieldDelegate>
+
+@property (strong, nonatomic) NSMutableArray *questionsArray;
+@property (weak, nonatomic) IBOutlet UITableView *questionsTableView;
+@property (strong, nonatomic) NSString *currentQuestion;
+
+@property (strong, nonatomic) IBOutlet UIView *addQuestionView;
+@property (weak, nonatomic) IBOutlet UITextView *addQuestionTextField;
+
 
 @end
 
 @implementation ViewController
+
+-(void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.addQuestionView.alpha = 0.0;
+}
 
 - (void)viewDidAppear:(BOOL)animated {
     [super viewDidAppear:NO];
@@ -20,8 +34,14 @@
     PFUser *currentUser = [PFUser currentUser];
     
     if (currentUser) {
-        // Display questions
-    
+        PFQuery *query = [PFQuery queryWithClassName:@"Question"];
+        [query findObjectsInBackgroundWithBlock:^(NSArray * _Nullable objects, NSError * _Nullable error) {
+            if (!error) {
+                self.questionsArray = [objects mutableCopy];
+                [self.questionsTableView reloadData];
+            }
+        }];
+        
     }
     else {
     PFLogInViewController *logInVC = [[PFLogInViewController alloc] init];
@@ -50,5 +70,73 @@
     [self presentViewController:logInVC animated:YES completion:nil];
 }
 
+-(IBAction)saveButtonPressed:(id)sender {
+    PFObject *question = [PFObject objectWithClassName:@"Question"];
+    question[@"questionText"] = self.addQuestionTextField.text;
+    
+    [question saveInBackgroundWithBlock:^(BOOL succeeded, NSError * _Nullable error) {
+        if (succeeded) {
+        }
+        else {
+            // Log error
+        }
+    }];
+    
+    PFQuery *query = [PFQuery queryWithClassName:@"Question"];
+    [query findObjectsInBackgroundWithBlock:^(NSArray * _Nullable objects, NSError * _Nullable error) {
+        if (!error) {
+            self.questionsArray = [objects mutableCopy];
+            [self.questionsTableView reloadData];
+        }
+    }];
+    
+    self.addQuestionView.alpha = 0.0;
+}
+
+-(IBAction)cancelButtonPressed:(id)sender {
+    
+    self.addQuestionView.alpha = 0.0;
+    
+}
+
+- (IBAction)newQuestionButtonPressed:(UIBarButtonItem *)sender {
+    
+    [UIView animateWithDuration:0.4 animations:^{
+        
+        self.addQuestionView.alpha = 1.0;
+        
+    }];
+    
+    
+}
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"cell"];
+    
+    PFObject *questions = self.questionsArray[indexPath.row];
+    cell.textLabel.text = questions[@"questionText"];
+    
+    return cell; 
+}
+
+
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return [self.questionsArray count];
+}
+
+-(BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath{
+    return YES;
+}
+
+-(void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath{
+    
+    PFObject *questions = self.questionsArray[indexPath.row];
+    [questions deleteInBackground];
+    [questions saveInBackground];
+
+    [self.questionsArray removeObjectAtIndex:indexPath.row];
+    [self.questionsTableView reloadData];
+    
+}
 
 @end
