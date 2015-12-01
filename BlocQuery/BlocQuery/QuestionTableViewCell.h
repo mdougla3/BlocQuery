db69dc8a134b8383936ec4b2bb82c3493d01f9ef
mdougla3 <McCay@mccays-macbook-pro.local>
2015-11-30 20:24:52 -0600

    Added cell attributes

---
+++ BlocQuery/BlocQuery/QuestionTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  QuestionTableViewCell.h
+//  BlocQuery
+//
+//  Created by McCay Barnes on 11/30/15.
+//  Copyright © 2015 McCay Barnes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface QuestionTableViewCell : UITableViewCell
+
+@end
---
+++ BlocQuery/BlocQuery/QuestionTableViewCell.m
@@ -0,0 +1,26 @@
+//
+//  QuestionTableViewCell.m
+//  BlocQuery
+//
+//  Created by McCay Barnes on 11/30/15.
+//  Copyright © 2015 McCay Barnes. All rights reserved.
+//
+
+#import "QuestionTableViewCell.h"
+
+@implementation QuestionTableViewCell
+
+
+
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
